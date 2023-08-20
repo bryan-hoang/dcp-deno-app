245e275a5f23380be4057c27bfece144c6b8325e
Bryan Hoang <bryan@bryanhoang.dev>
2023-08-19 20:11:01 -0400

    feat: add initial code to run `dcp-client`

---
+++ main.ts
@@ -0,0 +1,12 @@
+import dcpClient from 'npm:dcp-client';
+
+console.log(dcpClient);
+
+export function add(a: number, b: number): number {
+	return a + b;
+}
+
+// Learn more at https://deno.land/manual/examples/module_metadata#concepts
+if (import.meta.main) {
+	console.log('Add 2 + 3 =', add(2, 3));
+}
